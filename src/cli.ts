abf19a4ff397409b5f7f2c5ac15ae654434b8435
harrytang <harry@powerkernel.com>
2022-03-24 17:44:58 +0200

    feat: refactor and add some templates

--- src/cli.ts
+++ src/cli.ts
@@ -6,42 +6,125 @@
 
 import fs from 'fs';
 import { TwingEnvironment, TwingLoaderFilesystem } from 'twing';
-import { mkdirs } from './helper';
+import { mkdirs, generateNames, Names } from './helper';
 import path from 'path';
 
-const cli = async (): Promise<void> => {
-  const args = process.argv.slice(2);
-
+const validateArgs = (args: string[]) => {
   if (args.length !== 1) {
     console.error('missing arguments');
     process.exit(1);
   }
+};
 
-  const entity = args[0];
-
-  // write dirs
-  mkdirs(entity);
-
-  // template
-  let loader = new TwingLoaderFilesystem(`${__dirname}/../templates`);
+const generateDtos = (names: Names) => {
+  let loader = new TwingLoaderFilesystem(`${__dirname}/../templates/dtos`);
   let twing = new TwingEnvironment(loader);
 
-  // entity
-  twing.render('entities/entity.twig', { Entity: entity, EntityDto: `${entity}Dto` }).then((output) => {
+  // entity dto
+  twing.render(`entity-dto.twig`, { Class: names.dtos.entity.class }).then((output) => {
     try {
-      fs.writeFileSync(`./src/domains/${entity.toLowerCase()}/entities/${entity.toLowerCase()}.ts`, output);
+      fs.writeFileSync(`./src/domains/${names.entity.file}/dtos/${names.dtos.entity.file}.ts`, output);
     } catch (err) {
       console.error(err);
     }
   });
+
+  // create entity dto
+  twing.render(`create-entity-dto.twig`, { Class: names.dtos.create.class }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/dtos/${names.dtos.create.file}.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // list entity dto
+  twing.render(`list-entity-dto.twig`, { Class: names.dtos.list.class, Entity: names.entity.class }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/dtos/${names.dtos.list.file}.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // update
+  twing.render(`update-entity-dto.twig`, { Class: names.dtos.update.class }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/dtos/${names.dtos.update.file}.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // index
   twing
-    .render('entities/index.twig', { Entity: entity, EntityFile: `${entity.toLocaleLowerCase()}` })
+    .render('index.twig', {
+      EntityDtoClass: names.dtos.entity.class,
+      EntityDtoFile: names.dtos.entity.file,
+      CreateEntityDtoClass: names.dtos.create.class,
+      CreateEntityDtoFile: names.dtos.create.file,
+      UpdateEntityDtoClass: names.dtos.update.class,
+      UpdateEntityDtoFile: names.dtos.update.file,
+      ListEntityDtoClass: names.dtos.list.class,
+      ListEntityDtoFile: names.dtos.list.file,
+    })
     .then((output) => {
       try {
-        fs.writeFileSync(`./src/domains/${entity.toLowerCase()}/entities/index.ts`, output);
+        fs.writeFileSync(`./src/domains/${names.entity.file}/dtos/index.ts`, output);
       } catch (err) {
         console.error(err);
       }
     });
+};
+
+const generateEntiry = (names: Names) => {
+  let loader = new TwingLoaderFilesystem(`${__dirname}/../templates/entities`);
+  let twing = new TwingEnvironment(loader);
+
+  // entity
+  twing.render('entity.twig', { Class: names.entity.class, EntityDto: names.dtos.entity.class }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/entities/${names.entity.file}.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // inedx
+  twing.render('index.twig', { EntityClass: names.entity.class, EntityFile: names.entity.file }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/entities/index.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // entity test
+  twing.render('__tests__/entity.test.twig', { Class: names.entity.class }).then((output) => {
+    try {
+      fs.writeFileSync(`./src/domains/${names.entity.file}/entities/__tests__/${names.entity.file}.test.ts`, output);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+};
+
+const cli = async (): Promise<void> => {
+  // inputs
+  const args = process.argv.slice(2);
+  validateArgs(args);
+  const entity = args[0];
+
+  // names
+  const names = generateNames(entity);
+
+  // write dirs
+  mkdirs(entity);
+
+  // generate dtos
+  generateDtos(names);
+
+  // entity
+  generateEntiry(names);
 };
 export default cli;
--- src/helper.ts
+++ src/helper.ts
@@ -5,7 +5,53 @@
  */
 import fs from 'fs';
 
-const mkdirs = (name: string) => {
+interface FileClass {
+  file: string;
+  class: string;
+}
+
+interface Names {
+  entity: FileClass;
+  dtos: {
+    entity: FileClass;
+    create: FileClass;
+    list: FileClass;
+    update: FileClass;
+  };
+  controllers: {
+    count: FileClass;
+    create: FileClass;
+    delete: FileClass;
+    list: FileClass;
+    update: FileClass;
+    view: FileClass;
+  };
+  actions: {
+    count: FileClass;
+    create: FileClass;
+    delete: FileClass;
+    list: FileClass;
+    update: FileClass;
+    view: FileClass;
+  };
+  repo: {
+    create: FileClass;
+    delete: FileClass;
+    list: FileClass;
+    update: FileClass;
+    view: FileClass;
+  };
+  mongo: {
+    create: FileClass;
+    delete: FileClass;
+    list: FileClass;
+    update: FileClass;
+    view: FileClass;
+  };
+}
+
+const mkdirs = (entity: string) => {
+  const name = entity.toLowerCase();
   const dirs = [
     `./src/domains/${name.toLowerCase()}/entities/__tests__`,
     `./src/domains/${name.toLowerCase()}/dtos`,
@@ -22,4 +68,129 @@ const mkdirs = (name: string) => {
   });
 };
 
-export { mkdirs };
+const generateNames = (entity: string): Names => {
+  const name = entity.toLowerCase();
+
+  return {
+    entity: {
+      file: `${name}`,
+      class: entity,
+    },
+    dtos: {
+      entity: {
+        file: `${name}-dto`,
+        class: `${entity}Dto`,
+      },
+      create: {
+        file: `create-${name}-dto`,
+        class: `Create${entity}Dto`,
+      },
+      list: {
+        file: `list-${name}-dto`,
+        class: `List${entity}Dto`,
+      },
+      update: {
+        file: `update-${name}-dto`,
+        class: `Update${entity}Dto`,
+      },
+    },
+    controllers: {
+      count: {
+        file: `count-${name}-controller`,
+        class: `Count${entity}Controller`,
+      },
+      create: {
+        file: `create-${name}-controller`,
+        class: `Create${entity}Controller`,
+      },
+      delete: {
+        file: `delete-${name}-controller`,
+        class: `Delete${entity}Controller`,
+      },
+      list: {
+        file: `list-${name}-controller`,
+        class: `List${entity}Controller`,
+      },
+      update: {
+        file: `update-${name}-controller`,
+        class: `Update${entity}Controller`,
+      },
+      view: {
+        file: `view-${name}-controller`,
+        class: `View${entity}Controller`,
+      },
+    },
+    actions: {
+      count: {
+        file: `count-${name}-action`,
+        class: `Count${entity}Action`,
+      },
+      create: {
+        file: `create-${name}-action`,
+        class: `Create${entity}Action`,
+      },
+      delete: {
+        file: `delete-${name}-action`,
+        class: `Delete${entity}Action`,
+      },
+      list: {
+        file: `list-${name}-action`,
+        class: `List${entity}Action`,
+      },
+      update: {
+        file: `update-${name}-action`,
+        class: `Update${entity}Action`,
+      },
+      view: {
+        file: `view-${name}-action`,
+        class: `View${entity}Action`,
+      },
+    },
+    repo: {
+      create: {
+        file: `create-${name}-repo`,
+        class: `Create${entity}Repo`,
+      },
+      delete: {
+        file: `delete-${name}-repo`,
+        class: `Delete${entity}Repo`,
+      },
+      list: {
+        file: `list-${name}-repo`,
+        class: `List${entity}Repo`,
+      },
+      update: {
+        file: `update-${name}-repo`,
+        class: `Update${entity}Repo`,
+      },
+      view: {
+        file: `view-${name}-repo`,
+        class: `View${entity}Repo`,
+      },
+    },
+    mongo: {
+      create: {
+        file: `create-${name}-mongo-repo`,
+        class: `Create${entity}MongoRepo`,
+      },
+      delete: {
+        file: `delete-${name}-mongo-repo`,
+        class: `Delete${entity}MongoRepo`,
+      },
+      list: {
+        file: `list-${name}-mongo-repo`,
+        class: `List${entity}MongoRepo`,
+      },
+      update: {
+        file: `update-${name}-mongo-repo`,
+        class: `Update${entity}MongoRepo`,
+      },
+      view: {
+        file: `view-${name}-mongo-repo`,
+        class: `View${entity}MongoRepo`,
+      },
+    },
+  };
+};
+
+export { mkdirs, generateNames, Names };
